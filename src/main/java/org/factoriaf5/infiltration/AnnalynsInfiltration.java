bde6d363542cc350cf54e0cecaa3f684dbd2824f
lara-gs <lara_989@hotmail.com>
2024-10-29 18:14:29 +0100

    all methods created

--- src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
+++ src/main/java/org/factoriaf5/infiltration/AnnalynsInfiltration.java
@@ -4,19 +4,27 @@ public class AnnalynsInfiltration {
 
     public static boolean canFastAttack(boolean knightIsAwake) {
         // Implement method
+        return knightIsAwake == false;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
         // Implement method
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
         // Implement method
+        return prisonerIsAwake && !archerIsAwake;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
             boolean petDogIsPresent) {
         // Implement method
+        if(petDogIsPresent){
+            return !archerIsAwake;
+        } else {
+            return prisonerIsAwake && !knightIsAwake && !archerIsAwake;
+        }
     }
 
 }
